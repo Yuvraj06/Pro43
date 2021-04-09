86b91421f5a619e6cbe3cbe0f2fd9a88a8123107
Yuvraj06 <73378479+Yuvraj06@users.noreply.github.com>
2021-04-09 19:27:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,56 @@
-
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-function preload()
-{
-	
-}
-
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(400, 400);
+	angleMode(DEGREES);
 
 
-	engine = Engine.create();
-	world = engine.world;
 
-	//Create the Bodies Here.
-
-
-	Engine.run(engine);
-  
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
+	background(0);
+	translate(200, 200);
+	rotate(-90);
+
+	var hr = hour();
+	var mn = minute();
+	var sc = second();
+
+	strokeWeight(8);
+	noFill();
+
+	stroke("yellow");
+	var map1 = map(sc, 0, 60, 0, 360);
+	arc(0, 0, 300, 300, 0, map1);
+
+	stroke("lightGreen");
+	var map2 = map(mn, 0, 60, 0, 360);
+	arc(0, 0, 280, 280, 0, map2);
+
+	stroke("lightBlue");
+	var map3 = map(hr % 12, 0, 12, 0, 360);
+	arc(0, 0, 260, 260, 0, map3);
+
+	push();
+	rotate(map1);
+	stroke("yellow");
+	strokeWeight(3);
+	line(0, 0, 100, 0);
+	pop();
+
+	push();
+	rotate(map2);
+	stroke("lightGreen");
+	strokeWeight(6);
+	line(0, 0, 75, 0);
+	pop();
+
+	push();
+	rotate(map3);
+	stroke("lightBlue");
+	strokeWeight(6);
+	line(0, 0, 50, 0);
+	pop();
+
+
 }
-
-
-
